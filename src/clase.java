9a866599a913f02aa54617ef9431753ca75d0771
chemiya11 <josemaria.lozano@alumnos.uva.es>
2021-09-21 07:50:33 +0200

    he modificado

--- src/clase.java
+++ src/clase.java
@@ -3,7 +3,7 @@ public class clase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("es prueba");
+		System.out.println("es prueba modifcamdo");
 	}
 
 }
